ff4cb487ff13db9f57544cf59dd54d21a3d580ed
sudarshan <sudiiyengar@example.com>
2025-07-18 21:25:27 +0530

    Add GoogleTest integration: workflow and sample test for swap_xor

---
+++ Day_1/test_swap.c
@@ -0,0 +1,16 @@
+#include <gtest/gtest.h>
+
+// Example function to test (to be replaced with your real functions)
+extern "C" void swap_xor(int *a, int *b);
+
+TEST(SwapTest, BasicSwap) {
+    int x = 5, y = 10;
+    swap_xor(&x, &y);
+    EXPECT_EQ(x, 10);
+    EXPECT_EQ(y, 5);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
